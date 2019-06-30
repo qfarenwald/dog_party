1f00171f0bb0c046165f192f29ec769e764f3f91
Quinne Farenwald <q.farenwald@gmail.com>
2019-06-30 16:23:59 -0600

    Refactor js to remove unused variables

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-var header = document.querySelector('.header__span');
 var nameInput = document.querySelector('.header__input--name');
-console.log("var name", nameInput);
 var changeNameButton = document.querySelector('.header__input--button');
 
 changeNameButton.addEventListener('click', function() {
-  var name = nameInput.value;
-  document.querySelector('.header__span').innerHTML = (name);
+  document.querySelector('.header__span').innerHTML = (nameInput.value);
 });
 
-// var colorInput = document.querySelector('#color');
-// var getColorButton = document.querySelector('.get-color');
+
+//WORKING CODE 1 - close to JS III lesson
+// var header = document.querySelector('.header__span');
+// var nameInput = document.querySelector('.header__input--name');
+// console.log("var name", nameInput);
+// var changeNameButton = document.querySelector('.header__input--button');
 //
-// getColorButton.addEventListener('click', function () {
-//   var color = colorInput.value;
-//   console.log(color);
+// changeNameButton.addEventListener('click', function() {
+//   var name = nameInput.value;
+//   document.querySelector('.header__span').innerHTML = (name);
 // });
 
+//WORKING CODE 2
+// var changeNameButton = document.querySelector('.header__input--button');
+// var nameInput = document.querySelector('.header__input--name');
+//
+// changeNameButton.addEventListener('click', function() {
+//   document.querySelector('.header__span').innerHTML = (nameInput.value);
+// });
 
-// var nameInput = document.querySelector(".header__input--name").value;
+//NOT WORKING - But i think it should
+// var changeNameButton = document.querySelector('.header__input--button');
+// var nameInput = document.querySelector('.header__input--name');
+// var header = document.querySelector('.header__span');
+//
+// changeNameButton.addEventListener('click', function() {
+//   document.querySelector(header).innerHTML = (nameInput.value);
+// });
